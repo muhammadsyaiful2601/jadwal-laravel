62143a1bd2be7261d8b8c5d1a1ecc6e89fd2041e
muhammadsyaiful2601 <muhammadsyaiful2601@gmail.com>
2026-08-01 09:12:40 +0700

    buat deploy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
             SuperadminSeeder::class,
             RoomSeeder::class,
             SemesterSettingSeeder::class,
-            ScheduleSeeder::class,
         ]);
     }
 }
